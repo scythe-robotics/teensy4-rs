966ce548f2cbcb16b93ec48ea1c863f4c034869a
Sebastian Kuzminsky <seb@scytherobotics.com>
2025-05-12 17:21:08 -0600

    grow stack from 16 kB to 32 kB

--- build.rs
+++ build.rs
@@ -13,7 +13,7 @@ fn main() {
         .heap_size(16 * 1024)
         .heap_size_env_override("TEENSY4_HEAP_SIZE")
         .stack(Memory::Dtcm)
-        .stack_size(16 * 1024)
+        .stack_size(2 * 16 * 1024)
         .stack_size_env_override("TEENSY4_STACK_SIZE")
         .vectors(Memory::Dtcm)
         .text(Memory::Flash)
